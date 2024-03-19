803e1d21bdcf5e24fa49ab378afffb0a3f430dc3
Orangekun <amazingritro66@gmail.com>
2024-03-19 13:38:55 +0200

    installed jest and set up babelrc

--- src/app.mjs
+++ src/app.mjs
@@ -1,2 +1,14 @@
 import express from "express";
+import { config } from "dotenv";
 const app = express()
+config()
+app.use(express.json())
+
+app.get('/api/v1/status', (req, res) => {
+    res.sendStatus(200)
+})
+
+
+app.listen(process.env.PORT, () => {
+    console.log('Listening on port: ' + process.env.PORT)
+})
